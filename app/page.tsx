a769acd8751458aa3e2ca3e302e6cd896371c426
Muraduzzaman <muraduzzaman@gmail.com>
2026-05-03 18:45:27 +0600

    Remove testimonials section per request

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,6 @@ import PremiumExperience from "./components/PremiumExperience";
 import Curriculum from "./components/Curriculum";
 import CourseOutline from "./components/CourseOutline";
 import Registration from "./components/Registration";
-import Testimonials from "./components/Testimonials";
 import SectionBlend from "./components/SectionBlend";
 
 export default function Home() {
@@ -15,8 +14,6 @@ export default function Home() {
       <SectionBlend from="emerald-950" to="slate-50" />
       <Curriculum />
       <SectionBlend from="slate-50" to="emerald-50" />
-      <Testimonials />
-      <SectionBlend from="emerald-50" to="emerald-50" />
       <CourseOutline />
       <SectionBlend from="emerald-50" to="emerald-900" />
       <Registration />
